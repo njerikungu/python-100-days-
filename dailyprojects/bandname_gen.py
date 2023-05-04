1d6bee4983dbb7e80f23d122167cd5eb3f031fdb
njerikungu <111002283+njerikungu@users.noreply.github.com>
2023-05-04 12:33:44 +0000

    Day 1

---
+++ dailyprojects/bandname_gen.py
@@ -0,0 +1,5 @@
+name = input("what's your name?\n")
+print(f"Welcome on board {name}!")
+city = input("What city did you grow up in?\n")
+pet = input("What's the name of your pet?\n")
+print(f"Your band name is {city} {pet}!")
