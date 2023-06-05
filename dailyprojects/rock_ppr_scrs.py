8c9d683bc4eb440f6fe8836d9930dd56dc979be5
njerikungu <111002283+njerikungu@users.noreply.github.com>
2023-06-05 08:22:27 +0000

    Day 4

---
+++ dailyprojects/rock_ppr_scrs.py
@@ -0,0 +1,10 @@
+#a rock paper scissors with the computer
+
+import random
+
+R = Rock 
+P = Paper
+S = Scissors
+my_choice = input(What do you choose? Input R for Rock, P for Paper or S for scissors)
+
+computer choice = random.randint(0, 2)
