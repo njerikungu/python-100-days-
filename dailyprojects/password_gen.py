603d0f099509e616164f5b9725b2b06f2ea7ceb9
njerikungu <111002283+njerikungu@users.noreply.github.com>
2023-06-07 07:14:24 +0000

    Day 5

---
+++ dailyprojects/password_gen.py
@@ -0,0 +1,8 @@
+#password generator
+#generates random password
+
+caps_letters = ['A', 'B', 'C', 'D']
+sml_letters = ['a', 'b', 'c', 'd']
+letters = [caps_letters, sml_letters]
+symbols = ['!', '@', '#', '$', '*']
+
